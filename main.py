223d7bd18eae24b38aba25fabfa3015493a5173c
Tanner Skelton <tskelton@huskers.unl.edu>
2020-11-19 13:24:42 -0600

    Finished script to generate a db scan on specified stats.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import csv
-import math
 import numpy as np
 import matplotlib.pyplot as plt
+
 import sys
 from sklearn.preprocessing import StandardScaler
-import pandas as pd
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import make_blobs
+from sklearn import metrics
 
 pokemon_objs = []
 class Pokemon(object):
@@ -104,11 +106,93 @@ def find_avg_stat(pokemon_objs, stat):
     stat_avg = total/length
     return round(stat_avg, 2)
 
+def add_against_stats(pokemon):
+    stat = float(pokemon.against_bug)
+    stat += float(pokemon.against_dark)
+    stat += float(pokemon.against_dragon)
+    stat += float(pokemon.against_electric)
+    stat += float(pokemon.against_fairy)
+    stat += float(pokemon.against_fight)
+    stat += float(pokemon.against_fire)
+    stat += float(pokemon.against_flying)
+    stat += float(pokemon.against_ghost)
+    stat += float(pokemon.against_grass)
+    stat += float(pokemon.against_ground)
+    stat += float(pokemon.against_ice)
+    stat += float(pokemon.against_normal)
+    stat += float(pokemon.against_psychic)
+    stat += float(pokemon.against_rock)
+    stat += float(pokemon.against_steel)
+    stat += float(pokemon.against_water)
+    return float(stat)
+
+def create_against_defense_cluster_data(pokemon_obj):
+    x_cords = []
+    y_cords = []
+    for pokemon in pokemon_objs:
+        x = pokemon.defense
+        y = add_against_stats(pokemon)
+        x_cords.append(float(x))
+        y_cords.append(y)
+    coords = generate_x(x_cords, y_cords)
+    return coords
+
+# create a 2D array given all the cords
+def generate_x(x_cords, y_cords):
+    x = []
+    for x_cords, y_cords in zip(x_cords, y_cords):
+        x.append([x_cords, y_cords])
+    return x
+
+def create_x_y(pokemon_objs):
+    x_cords = []
+    y_cords = []
+    for pokemon in pokemon_objs:
+        # this is where you can choose what to put in x and y
+        x_cords.append(float(pokemon.speed))
+        y_cords.append(float(pokemon.defense))
+    x = generate_x(x_cords, y_cords)
+    return x
+
+def generate_db_scan(pokemon_objs, stat1, stat2):
+    # specify the stats you want to make the model for in create_x_y
+    #centers = create_x_y(pokemon_objs)
+    # use this one if you want to cluster defense with all the against_
+    centers = create_against_defense_cluster_data(pokemon_objs)
+    X, labels_true = make_blobs(n_samples=800, centers=centers, cluster_std=0.5, random_state=0)
+    X = StandardScaler().fit_transform(X)
+    db = DBSCAN(eps=.3, min_samples=5).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    labels = db.labels_
+
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    #plot the result
+    unique_labels = set(labels)
+    colors = [plt.cm.get_cmap("Spectral")(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # noise
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=14)
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=6)
+    plt.xlabel(f'{stat1}')
+    plt.ylabel(f'{stat2}')
+    plt.show()
+
 def main():
     Pokemon.generate_pokemon()
-    #for pokemon in pokemon_objs:
-    #    print(pokemon.name)
     test = find_avg_stat(pokemon_objs, "hp")
+    # need to specify what to label x/y
+    generate_db_scan(pokemon_objs, "defense", "against_stats_combined")
     print(test)
 
 if __name__ == "__main__":
